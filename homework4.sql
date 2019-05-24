0715a60ce2d9c789f36a59a26cf1a87fe215989a
dinamizame <49082588+dinamizame@users.noreply.github.com>
2019-05-24 12:04:09 +0200

    agrego cambio hasta
    SELECT user_id, user_acquisition_timestamp,transaction_timestamp, 
    DATEDIFF(DAY, user_acquisition_timestamp, transaction_timestamp)as dias_dif
    FROM
    (SELECT user_id, user_acquisition_timestamp, transaction_timestamp
    from tangarana.payment_transactions
    WHERE user_acquisition_timestamp >'2017-01-01 00:00:.000' and transaction_timestamp <'2017-07-01 00:00:.000') AS TABLA1 
    WHERE dias_dif<=30
    order by 1,3;

--- homework4.sql
+++ homework4.sql
@@ -1,8 +1,9 @@
-SELECT  user_id, user_acquisition_timestamp,transaction_timestamp, 
+
+SELECT user_id, user_acquisition_timestamp,transaction_timestamp, 
 DATEDIFF(DAY, user_acquisition_timestamp, transaction_timestamp)as dias_dif
 FROM
 (SELECT user_id, user_acquisition_timestamp, transaction_timestamp
 from tangarana.payment_transactions
-WHERE user_acquisition_timestamp >'2017-01-01 00:00:.000') AS TABLA1
+WHERE user_acquisition_timestamp >'2017-01-01 00:00:.000' and transaction_timestamp <'2017-07-01 00:00:.000') AS TABLA1 
 WHERE dias_dif<=30
 order by 1,3;
