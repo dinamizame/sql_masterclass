607c7a4dc269a40b61341605ea0e765ce1c4ca96
dinamizame <49082588+dinamizame@users.noreply.github.com>
2019-05-24 19:48:03 +0200

    cambio con nvl
    SELECT t.user_id, transaction_id, visit_id, transaction_time, visit_time
    FROM
    (SELECT user_id, visit_id, visit_time,
    nvl (LEAD(visit_time,1) OVER (PARTITION BY user_id ORDER by visit_time), '2099-01-01')AS next_visit
    FROM sql_masterclass.visits
    order by 1,3) as nv
    left JOIN
    sql_masterclass.transactions t on nv.user_id=t.user_id
    WHERE transaction_time > visit_time and transaction_time < next_visit;

--- homework5.sql
+++
@@ -1,9 +0,0 @@
-SELECT t.user_id, transaction_id, visit_id, transaction_time, visit_time
-FROM
-(SELECT user_id, visit_id, visit_time,
-LEAD(visit_time,1) OVER (PARTITION BY user_id ORDER by visit_time) AS next_visit
-FROM sql_masterclass.visits
-order by 1,3) as nv
-INNER JOIN
-sql_masterclass.transactions t on nv.user_id=t.user_id
-WHERE transaction_time > visit_time and transaction_time < next_visit;
